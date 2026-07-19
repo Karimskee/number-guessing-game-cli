ca4307d7da8994d6c4c5f85da0ba43efe0c731c7
Karimskee <kareemgamer2006@gmail.com>
2026-07-19 06:08:27 +0300

    feat: textual welcoming screen

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ correctly, the game will end, and the user will win. Otherwise, the game will
 continue until the user runs out of chances.
 """
 
+# RUN COMMAND
+# textual-hmr app.py:GuessingGameApp
+
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.reactive import reactive
+from textual.containers import HorizontalGroup, VerticalScroll, VerticalGroup
+from textual.widgets import Static, Input, Select, Button, Label, Footer, Header
+
 from getpass import getuser  # Getting system username
 import json
 from math import floor
@@ -14,18 +23,6 @@ from random import randint
 import time  # Calculating round time
 
 from helpers import clear_terminal
-
-
-# Difficulties dictionary
-difficulty_levels = {
-    1: {"title": "Easy", "chances": 10, "multiplier": 1},
-    2: {"title": "Medium", "chances": 5, "multiplier": 2},
-    3: {"title": "Hard", "chances": 3, "multiplier": 3},
-}
-
-round_difficulty = {}  # Chosen difficulty level for each round
-SCORES_FILE = "scores.json"
-hint_on_chance = -1  # Give a hint once rem chances is equal to this value
 
 
 def main():
@@ -243,5 +240,106 @@ def score(attempts: int, round_time: float):
         print(f"Score has been saved with the name: {getuser()}")
 
 
+class WelcomeScreen(Screen):
+    """Welcoming screen."""
+
+    def compose(self) -> ComposeResult:
+        """Called to add widgets to this container."""
+        with VerticalScroll():
+            yield Label("Welcome to the Number Guessing Game!", classes="header")
+            yield Label("I'm thinking of a number between 1 and 100.\nYou have some number of chances to guess the correct number.", classes="explain")
+            yield Label("Please choose a difficulty level", classes="difficulty-prompt")
+            with HorizontalGroup(classes="difficulty-options"):
+                for diff_level in self.app.difficulty_levels:
+                    yield DifficultyButton(diff_level)
+
+        self.start_prompt_flash()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        button_id = event.button.id
+
+        self.log(f"Button pressed ID: {button_id}")
+        self.log.debug("Hey")
+
+        match button_id:
+            case "easy-diff-btn":
+                self.app.round_difficulty = self.app.difficulty_levels[0]
+            case "medium-diff-btn":
+                self.app.round_difficulty = self.app.difficulty_levels[1]
+            case "hard-diff-btn":
+                self.app.round_difficulty = self.app.difficulty_levels[2]
+    
+        self.app.switch_screen("game")
+
+    def start_prompt_flash(self):
+        self.set_interval(1, self.flash_prompt)
+
+    def flash_prompt(self):
+        label = self.query_one(".difficulty-prompt", Label)
+        label.visible = not label.visible
+
+
+class GameScreen(Screen):
+    """Guessing game screen."""
+
+    def compose(self) -> ComposeResult:
+        """Called to add widgets to this container."""
+        yield Label("Successfully switched to game screen!")
+        yield Label(f"Round difficulty: {self.app.round_difficulty.get("title")}", classes="round-difficulty")
+
+
+class DifficultyButton(Button):
+    """Difficulty button widget."""
+
+    def __init__(self, diff_level: dict, **kwargs):
+        super().__init__(**kwargs)
+
+        t = diff_level.get("title")
+        ch = diff_level.get("chances")
+        co = diff_level.get("color")
+
+        variant = {
+            "green": "success",
+            "yellow": "warning",
+            "red": "error",
+        }.get(co, "default")
+
+        self.label = f"{t}\n[dim]{ch} {"chances" if ch > 1 else "chance"}[/]"
+        self.id = f"{t}-diff-btn"
+        self.variant = variant
+
+
+class GuessingGameApp(App):
+    """Managing the guessing game CLI app."""        
+
+    CSS_PATH = "style.tcss"
+
+    SCREENS = {
+        "welcome": WelcomeScreen,
+        "game": GameScreen
+    }
+
+    # Difficulties dictionary
+    difficulty_levels = [
+        {"title": "easy", "chances": 10, "multiplier": 1, "color": "green"},
+        {"title": "medium", "chances": 5, "multiplier": 2, "color": "yellow"},
+        {"title": "hard", "chances": 3, "multiplier": 3, "color": "red"},
+    ]
+
+    round_difficulty = {}  # Chosen difficulty level for each round
+
+    SCORES_FILE = "scores.json"
+    hint_on_chance = -1  # Give a hint once rem chances is equal to this value
+    
+    # def compose(self) -> ComposeResult:
+    #     """Called to add widgets to the app."""
+    #     yield Header()
+    #     yield Footer()
+
+    def on_mount(self):
+        self.push_screen(screen="welcome")
+
+
 if __name__ == "__main__":
-    main()
+    GuessingGameApp().run()
